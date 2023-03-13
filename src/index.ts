7faaba9b21909365f4ca9751a2c35059f93b6051
imrany <imranmat254gmail.com>
2023-03-13 13:14:03 +0300

    tpes in objects

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,18 @@ let isStudent: boolean
 isStudent=false
 //"any" allows the variable value to be any type
 let letter: any ="a" 
+
+//type in objects
+type detailtype={
+    id: number
+    name: string
+    single: boolean
+    desc?: string //by adding a "?" makes the property optional
+}
+
+const user:detailtype={
+    id:1,
+    name:"Imran",
+    single:true,
+    desc:"Hello there"
+}
